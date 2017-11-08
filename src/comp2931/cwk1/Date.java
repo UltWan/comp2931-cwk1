d2df25e52381f7d1e890bc18718082f3f87f140a
Ryan Wan <sc16wyrw@leeds.ac.uk>
2017-11-08 16:11:48 +0000

    gitignore test

--- src/comp2931/cwk1/Date.java
+++ src/comp2931/cwk1/Date.java
@@ -8,6 +8,9 @@ package comp2931.cwk1;
  */
 public class Date {
 
+  private static final int DAYS_PER_YEAR = 365;
+  private static final int DAYS_PER_LEAPYEAR = 366;
+
   private int year;
   private int month;
   private int day;
@@ -23,9 +26,27 @@ public class Date {
     year = y;
     month = m;
     day = d;
+
+    if (year < 1 || year > 9999) {
+      throw new IllegalArgumentException("Invalid year");
+    }
+
+    if (month < 1 || month > 12) {
+      throw new IllegalArgumentException("Invalid month");
+    }
+
+    if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day > 31)) {
+      throw new IllegalArgumentException("Invalid days for month");
+    }
+
+    if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30)) {
+      throw new IllegalArgumentException("Invalid days for month 2");
+    }
+
+    if ((month == 2) && (day < 1 || day > 28)) {
+      throw new IllegalArgumentException("Day does not exist in February");
+    }
   }
-
-
 
   /**
    * Returns the year component of this date.
@@ -55,6 +76,15 @@ public class Date {
   }
 
   /**
+   * Returns the day component of this date.
+   *
+   * @return Day
+   */
+  public int getDayofYear() {
+    return day;
+  }
+
+  /**
    * Provides a string representation of this date.
    *
    * ISO 8601 format is used (YYYY-MM-DD).
@@ -64,6 +94,33 @@ public class Date {
   @Override
   public String toString() {
     return String.format("%04d-%02d-%2d", year, month, day);
+  }
+
+  /**
+   * Tests whether this date is equal to another.
+   *
+   * <p>The two objects are considered equal if both are instances of
+   * the Date class <em>and</em> both represent exactly the same date
+   *
+   * @return True if this Date object is equal to the other, false otherwise
+   */
+  @Override
+  public boolean equals(Object other) {
+    if (other == this) {
+      // 'other' is same object as this one!
+      return true;
+    }
+    else if (! (other instanceof Date)) {
+      // 'other' is not a Date object
+      return false;
+    }
+    else {
+      // Compare fields
+      Date otherDate = (Date) other;
+      return getYear() == otherDate.getYear()
+          && getMonth() == otherDate.getMonth()
+          && getDay() == otherDate.getDay();
+    }
   }
 
 }
--- src/comp2931/cwk1/DateTest.java
+++ src/comp2931/cwk1/DateTest.java
@@ -14,23 +14,69 @@ import static org.hamcrest.CoreMatchers.*;
 public class DateTest {
 
   private Date middle;
-  private Date millenium;
+  private Date lastYear;
   private Date newYearsEve;
+  private Date wrongDays;
 
   @Before
   public void setUp() {
     middle = new Date(1975, 06, 15);
-    millenium = new Date(2000, 01, 01);
+    lastYear = new Date(9999, 12, 31);
     newYearsEve = new Date(1999, 12, 31);
   }
 
   @Test
   public void checkStringFormat() {
-    Date middle = new Date(1975, 06, 15);
     assertThat(middle.getYear(), is(1975));
     assertThat(middle.getMonth(), is(06));
     assertThat(middle.getDay(), is(15));
   }
 
+  @Test(expected=IllegalArgumentException.class)
+  public void checkValidDays() {
+    Date wrongDays = new Date(2000, 04, 31);
+    assertThat(wrongDays.getYear(), is(2000));
+    assertThat(wrongDays.getMonth(), is(04));
+    assertThat(wrongDays.getDay(), is(31));
+  }
 
+  @Test(expected=IllegalArgumentException.class)
+  public void yearsTooLow() {
+    new Date(0, 12, 15);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void yearsTooHigh() {
+    new Date(10000, 12, 15);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void monthsTooLow() {
+    new Date(2000, 0, 15);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void monthsTooHigh() {
+    new Date(2000, 13, 15);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void daysTooLow() {
+    new Date(2000, 12, 0);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void daysTooHigh() {
+    new Date(2000, 12, 32);
+  }
+
+  @Test
+  public void equality() {
+    assertTrue(newYearsEve.equals(newYearsEve));
+    assertTrue(newYearsEve.equals(new Date(1999, 12, 31)));
+    assertFalse(newYearsEve.equals(new Date(2000, 12, 31)));
+    assertFalse(newYearsEve.equals(new Date(1999, 1, 31)));
+    assertFalse(newYearsEve.equals(new Date(1999, 12, 1)));
+    assertFalse(newYearsEve.equals("1999/12/31"));
+  }
 }
